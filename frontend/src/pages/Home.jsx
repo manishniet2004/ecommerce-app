a4e246a1ae1684d5c67800089af29dfca359e321
Manish Sharma <manishniet2004@gmail.com>
2026-05-09 14:36:19 +0530

    Fix: Final case sensitivity for NewsLetterBox

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,8 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
-import NewsletterBox from '../components/NewsLetterBox'
+import NewsLetterBox from '../components/NewsletterBox'
+// import NewsletterBox from '../components/NewsLetterBox'
 
 const Home = () => {
   return (
